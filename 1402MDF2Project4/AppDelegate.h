0ef8ecbfa4524ca3681989f302b77511a757604d
eng618 <eng618@fullsail.edu>
2014-02-24 22:31:37 -0500

    Initial Commit

---
+++ 1402MDF2Project4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  1402MDF2Project4
+//
+//  Created by Eric Garcia on 2/24/14.
+//  Copyright (c) 2014 Full Sail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 1402MDF2Project4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  1402MDF2Project4
+//
+//  Created by Eric Garcia on 2/24/14.
+//  Copyright (c) 2014 Full Sail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
